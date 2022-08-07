00034c7a96b941f0bc3d3713ce3de2ea7ff95c15
Marcos8060 <marcosgav80@gmail.com>
2022-08-07 12:31:14 +0300

    created the first view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from .models import *
 
 # Create your views here.
+class Products(generics.ListCreatAPIView):
+    queryset = Product.ojects.all()
+    serializer_class = ProductSerializer
+
