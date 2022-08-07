83a9843fba7cce4c39a54bf0ecfe7fbe441e3765
Marcos8060 <marcosgav80@gmail.com>
2022-08-07 12:54:40 +0300

    installed cloudinary for image storage

--- deliveroo/settings.py
+++ deliveroo/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'cloudinary',
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'ochibe',
+    'API_KEY': '487191322497874',
+    'API_SECRET': 'HJca6uTCKCu_0zLVx7lSsXJm1fw',
+}
