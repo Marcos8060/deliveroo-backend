706c7c29e9d0edcedcca8f2f0ae2d96c226ba062
Marcos8060 <marcosgav80@gmail.com>
2022-08-07 12:07:22 +0300

    created the product model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images') # image
+    description = models.TextField(max_length=240)
+
+    def__str(self):
+        return self.name
+
