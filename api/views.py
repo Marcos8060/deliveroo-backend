2a161cf2ef59a4fa02120ba05b53dc5aa1139499
Marcos8060 <marcosgav80@gmail.com>
2022-08-10 13:03:51 +0300

    created user registration view

--- api/views.py
+++ api/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import *
 from .models import *
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+
+
+
+# user registration view
+class CustomUserCreate(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        reg_serializer = RegisterUserSerializer(data=request.data)
+        if reg_serializer.is_valid():
+            new_user = reg_serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
 
 # Create your views here.
 class Products(generics.ListCreateAPIView):
