2dcbdd0ebc423b6031f85d976608c0eb45350d4a
Marcos8060 <marcosgav80@gmail.com>
2022-08-10 13:00:32 +0300

    created a registration serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import *
+from django.contrib.auth.models import User
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=50,min_length=6,write_only=True)
+    # profiles = serializers.PrimaryKeyRelatedField(many=True, queryset=Profile.objects.all())
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+    
+    def create(self,validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
