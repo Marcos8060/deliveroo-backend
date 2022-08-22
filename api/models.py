955ee8a28908b093f1363ecc2587d96f94b1b2c0
Marcos8060 <marcosgav80@gmail.com>
2022-08-22 12:44:57 +0300

    made fresh database migrations

--- api/models.py
+++ api/models.py
@@ -7,7 +7,8 @@ class Product(models.Model):
     price = models.CharField(max_length=30)
     image = models.ImageField(upload_to='images') # image
     description = models.TextField(max_length=240)
-
+    quantity = models.IntegerField(default=1)
+    
     def __str__(self):
         return self.name
 
